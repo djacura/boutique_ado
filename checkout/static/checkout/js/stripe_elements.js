d67bc8bf816a0bc542c17d3ff52bff87fb5aa1f2
Daryl <d.jacura@sky.com>
2020-12-15 20:07:32 +0000

    changed API key settings in gitpod

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -10,21 +10,20 @@ var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
-  base: {
-    color: '#000',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
     }
-  },
-  invalid: {
-    color: '#dc3545',
-    iconColor: '#dc3545'
-  }
 };
-
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
@@ -44,38 +43,36 @@ card.addEventListener('change', function (event) {
     }
 });
 
-// handle form submit
-
+// Handle form submit
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
-  ev.preventDefault();
-  card.update({ 'disabled': true});
-  $('#submit-button').attr('disabled', true);
-  $('#payment-form').fadeToggle(100);
-  $('#loading-overlay').fadeToggle(100);
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-    }
-  }).then(function(result) {
-    if (result.error) {
-        var errorDiv = document.getElementById('card-errors');
-        var html = `
-                <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>
-            `;
-        $(errorDiv).html(html);
-        $('#payment-form').fadeToggle(100);
-        $('#loading-overlay').fadeToggle(100);
-        card.update({ 'disabled': false});
-        $('#submit-button').attr('disabled', false);
-    } else {
-        if (result.paymentIntent.status === 'succeeded') {
-            form.submit();
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
         }
-    }
-  });
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
